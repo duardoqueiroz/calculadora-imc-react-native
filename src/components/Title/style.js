c2bb573901e48045d7a942c0e64a3838906b8624
duardoqueiroz <eduardoldq1@gmail.com>
2023-05-30 16:55:29 -0300

    feat: Title component

---
+++ src/components/Title/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boxTitle: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  textTitle: {
+    color: "#000000",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;

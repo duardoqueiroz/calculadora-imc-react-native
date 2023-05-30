187ef19d2bc94c430ddf66492248e3c4a8138c40
duardoqueiroz <eduardoldq1@gmail.com>
2023-05-30 16:55:37 -0300

    feat: Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import styles from "./style";
+import { Pressable, Text } from "react-native";
+
+export default function Button(props) {
+  const { onPress, title } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
---
+++ src/components/Button/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+    paddingHorizontal: 50,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.4,
+    color: "white",
+  },
+});
+
+export default styles;
